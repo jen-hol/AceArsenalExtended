2d49ec1c3eba42317404a84a2ab592828226e286
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2022-01-21 16:04:02 +0000

    feat: Update ACR - Use new CUP options and add UGL

--- compats/compat_cup_weapons/infos/acr.hpp
+++ compats/compat_cup_weapons/infos/acr.hpp
@@ -1,167 +1,318 @@
 class CUP_arifle_ACR_blk_556 {
 	model = "CUP_ACR";
 	camo = "BLK";
-	caliber = "cal_556";
-	length = "standard";
+	caliber = "Caliber556";
+	length = "Standard";
+	ugl = "None";
 };
 
 class CUP_arifle_ACR_blk_68 {
 	model = "CUP_ACR";
 	camo = "BLK";
-	caliber = "cal_68";
-	length = "standard";
+	caliber = "Caliber68";
+	length = "Standard";
+	ugl = "None";
 };
 
 class CUP_arifle_ACR_snw_556 {
 	model = "CUP_ACR";
-	camo = "CUP_SNW";
-	caliber = "cal_556";
-	length = "standard";
+	camo = "SNW";
+	caliber = "Caliber556";
+	length = "Standard";
+	ugl = "None";
 };
 
 class CUP_arifle_ACR_snw_68 {
 	model = "CUP_ACR";
-	camo = "CUP_SNW";
-	caliber = "cal_68";
-	length = "standard";
+	camo = "SNW";
+	caliber = "Caliber68";
+	length = "Standard";
+	ugl = "None";
 };
 
 class CUP_arifle_ACR_tan_556 {
 	model = "CUP_ACR";
 	camo = "TAN";
-	caliber = "cal_556";
-	length = "standard";
+	caliber = "Caliber556";
+	length = "Standard";
+	ugl = "None";
 };
 
 class CUP_arifle_ACR_tan_68 {
 	model = "CUP_ACR";
 	camo = "TAN";
-	caliber = "cal_68";
-	length = "standard";
+	caliber = "Caliber68";
+	length = "Standard";
+	ugl = "None";
 };
 
 class CUP_arifle_ACR_wdl_556 {
 	model = "CUP_ACR";
-	camo = "CUP_WDL";
-	caliber = "cal_556";
-	length = "standard";
+	camo = "WDL";
+	caliber = "Caliber556";
+	length = "Standard";
+	ugl = "None";
 };
 
 class CUP_arifle_ACR_wdl_68 {
 	model = "CUP_ACR";
-	camo = "CUP_WDL";
-	caliber = "cal_68";
-	length = "standard";
+	camo = "WDL";
+	caliber = "Caliber68";
+	length = "Standard";
+	ugl = "None";
 };
 
 class CUP_arifle_ACRC_blk_556 {
 	model = "CUP_ACR";
 	camo = "BLK";
-	caliber = "cal_556";
-	length = "compact";
+	caliber = "Caliber556";
+	length = "Compact";
+	ugl = "None";
 };
 
 class CUP_arifle_ACRC_blk_68 {
 	model = "CUP_ACR";
 	camo = "BLK";
-	caliber = "cal_68";
-	length = "compact";
+	caliber = "Caliber68";
+	length = "Compact";
+	ugl = "None";
 };
 
 class CUP_arifle_ACRC_snw_556 {
 	model = "CUP_ACR";
-	camo = "CUP_SNW";
-	caliber = "cal_556";
-	length = "compact";
+	camo = "SNW";
+	caliber = "Caliber556";
+	length = "Compact";
 };
 
 class CUP_arifle_ACRC_snw_68 {
 	model = "CUP_ACR";
-	camo = "CUP_SNW";
-	caliber = "cal_68";
-	length = "compact";
+	camo = "SNW";
+	caliber = "Caliber68";
+	length = "Compact";
+	ugl = "None";
 };
 
 class CUP_arifle_ACRC_tan_556 {
 	model = "CUP_ACR";
 	camo = "TAN";
-	caliber = "cal_556";
-	length = "compact";
+	caliber = "Caliber556";
+	length = "Compact";
+	ugl = "None";
 };
 
 class CUP_arifle_ACRC_tan_68 {
 	model = "CUP_ACR";
 	camo = "TAN";
-	caliber = "cal_68";
-	length = "compact";
+	caliber = "Caliber68";
+	length = "Compact";
+	ugl = "None";
 };
 
 class CUP_arifle_ACRC_wdl_556 {
 	model = "CUP_ACR";
-	camo = "CUP_WDL";
-	caliber = "cal_556";
-	length = "compact";
+	camo = "WDL";
+	caliber = "Caliber556";
+	length = "Compact";
+	ugl = "None";
 };
 
 class CUP_arifle_ACRC_wdl_68 {
 	model = "CUP_ACR";
-	camo = "CUP_WDL";
-	caliber = "cal_68";
-	length = "compact";
+	camo = "WDL";
+	caliber = "Caliber68";
+	length = "Compact";
+	ugl = "None";
 };
 
 class CUP_arifle_ACR_DMR_blk_556 {
 	model = "CUP_ACR";
 	camo = "BLK";
-	caliber = "cal_556";
-	length = "dmr";
+	caliber = "Caliber556";
+	length = "DMR";
+	ugl = "None";
 };
 
 class CUP_arifle_ACR_DMR_blk_68 {
 	model = "CUP_ACR";
 	camo = "BLK";
-	caliber = "cal_68";
-	length = "dmr";
+	caliber = "Caliber68";
+	length = "DMR";
+	ugl = "None";
 };
 
 class CUP_arifle_ACR_DMR_snw_556 {
 	model = "CUP_ACR";
-	camo = "CUP_SNW";
-	caliber = "cal_556";
-	length = "dmr";
+	camo = "SNW";
+	caliber = "Caliber556";
+	length = "DMR";
+	ugl = "None";
 };
 
 class CUP_arifle_ACR_DMR_snw_68 {
 	model = "CUP_ACR";
-	camo = "CUP_SNW";
-	caliber = "cal_68";
-	length = "dmr";
+	camo = "SNW";
+	caliber = "Caliber68";
+	length = "DMR";
+	ugl = "None";
 };
 
 class CUP_arifle_ACR_DMR_tan_556 {
 	model = "CUP_ACR";
 	camo = "TAN";
-	caliber = "cal_556";
-	length = "dmr";
+	caliber = "Caliber556";
+	length = "DMR";
+	ugl = "None";
 };
 
 class CUP_arifle_ACR_DMR_tan_68 {
 	model = "CUP_ACR";
 	camo = "TAN";
-	caliber = "cal_68";
-	length = "dmr";
+	caliber = "Caliber68";
+	length = "DMR";
+	ugl = "None";
 };
 
 class CUP_arifle_ACR_DMR_wdl_556 {
 	model = "CUP_ACR";
-	camo = "CUP_WDL";
-	caliber = "cal_556";
-	length = "dmr";
+	camo = "WDL";
+	caliber = "Caliber556";
+	length = "DMR";
+	ugl = "None";
 };
 
 class CUP_arifle_ACR_DMR_wdl_68 {
 	model = "CUP_ACR";
-	camo = "CUP_WDL";
-	caliber = "cal_68";
-	length = "dmr";
+	camo = "WDL";
+	caliber = "Caliber68";
+	length = "DMR";
+	ugl = "None";
+};
+
+class CUP_arifle_ACR_EGLM_blk_556 {
+	model = "CUP_ACR";
+	camo = "BLK";
+	caliber = "Caliber556";
+	length = "Standard";
+	ugl = "EGLM";
+};
+
+class CUP_arifle_ACR_EGLM_blk_68 {
+	model = "CUP_ACR";
+	camo = "BLK";
+	caliber = "Caliber68";
+	length = "Standard";
+	ugl = "EGLM";
+};
+
+class CUP_arifle_ACR_EGLM_tan_556 {
+	model = "CUP_ACR";
+	camo = "TAN";
+	caliber = "Caliber556";
+	length = "Standard";
+	ugl = "EGLM";
+};
+
+class CUP_arifle_ACR_EGLM_tan_68 {
+	model = "CUP_ACR";
+	camo = "TAN";
+	caliber = "Caliber68";
+	length = "Standard";
+	ugl = "EGLM";
+};
+
+class CUP_arifle_ACR_EGLM_wdl_556 {
+	model = "CUP_ACR";
+	camo = "WDL";
+	caliber = "Caliber556";
+	length = "Standard";
+	ugl = "EGLM";
+};
+
+class CUP_arifle_ACR_EGLM_wdl_68 {
+	model = "CUP_ACR";
+	camo = "WDL";
+	caliber = "Caliber68";
+	length = "Standard";
+	ugl = "EGLM";
+};
+
+class CUP_arifle_ACR_EGLM_snw_556 {
+	model = "CUP_ACR";
+	camo = "SNW";
+	caliber = "Caliber556";
+	length = "Standard";
+	ugl = "EGLM";
+};
+
+class CUP_arifle_ACR_EGLM_snw_68 {
+	model = "CUP_ACR";
+	camo = "SNW";
+	caliber = "Caliber68";
+	length = "Standard";
+	ugl = "EGLM";
+};
+
+class CUP_arifle_ACRC_EGLM_blk_556 {
+	model = "CUP_ACR";
+	camo = "BLK";
+	caliber = "Caliber556";
+	length = "Compact";
+	ugl = "EGLM";
+};
+
+class CUP_arifle_ACRC_EGLM_blk_68 {
+	model = "CUP_ACR";
+	camo = "BLK";
+	caliber = "Caliber68";
+	length = "Compact";
+	ugl = "EGLM";
+};
+
+class CUP_arifle_ACRC_EGLM_tan_556 {
+	model = "CUP_ACR";
+	camo = "TAN";
+	caliber = "Caliber556";
+	length = "Compact";
+	ugl = "EGLM";
+};
+
+class CUP_arifle_ACRC_EGLM_tan_68 {
+	model = "CUP_ACR";
+	camo = "TAN";
+	caliber = "Caliber68";
+	length = "Compact";
+	ugl = "EGLM";
+};
+
+class CUP_arifle_ACRC_EGLM_wdl_556 {
+	model = "CUP_ACR";
+	camo = "WDL";
+	caliber = "Caliber556";
+	length = "Compact";
+	ugl = "EGLM";
+};
+
+class CUP_arifle_ACRC_EGLM_wdl_68 {
+	model = "CUP_ACR";
+	camo = "WDL";
+	caliber = "Caliber68";
+	length = "Compact";
+	ugl = "EGLM";
+};
+
+class CUP_arifle_ACRC_EGLM_snw_556 {
+	model = "CUP_ACR";
+	camo = "SNW";
+	caliber = "Caliber556";
+	length = "Compact";
+	ugl = "EGLM";
+};
+
+class CUP_arifle_ACRC_EGLM_snw_68 {
+	model = "CUP_ACR";
+	camo = "SNW";
+	caliber = "Caliber68";
+	length = "Compact";
+	ugl = "EGLM";
 };
--- compats/compat_cup_weapons/models/acr.hpp
+++ compats/compat_cup_weapons/models/acr.hpp
@@ -3,60 +3,19 @@ class CUP_ACR {
 	author = "$STR_ACEAX_CUP_author";
 	options[] = {"camo", "caliber", "length", "ugl"};
 
-	class camo {
-		values[] = {"BLK", "CUP_SNW", "TAN", "CUP_WDL"};
-
-		class WHT;
-		class CUP_SNW: WHT {
-			label = "$STR_ACEAX_CUP_SNW_dn";
-			image="#(rgb,8,8,3)color(0.9,0.9,0.9,1)";
-		};
-
-		class CUP_WDL {
-			label = "$STR_ACEAX_CUP_WDL_dn";
-			description = "$STR_ACEAX_CUP_WDL_dsc";
-			image="#(rgb,8,8,3)color(0.02,0.39,0.30,1)";
-		};
+	class camo: CUPCamo {
+		values[] = {"BLK", "SNW", "TAN", "WDL"};
 	};
 
-	class caliber {
-		label = "$STR_ACEAX_CUP_caliber_dn";
-		changeingame = 0;
-
-		values[] = {"cal_556", "cal_68"};
-
-		class cal_556 {
-			label = "$STR_ACEAX_CUP_cal_556_dn";
-			description = "$STR_ACEAX_CUP_cal_556_dsc";
-			image="#(rgb,8,8,3)color(0.1,0.1,0.1,1)";
-		};
-
-		class cal_68 {
-			label = "$STR_ACEAX_CUP_cal_68_dn";
-			description = "$STR_ACEAX_CUP_cal_68_dsc";
-			image="#(rgb,8,8,3)color(0.1,0.1,0.1,1)";
-		};
+	class caliber: CUPCaliber {
+		values[] = {"Caliber556", "Caliber68"};
 	};
 
-	class length {
-		label = "$STR_ACEAX_CUP_length_dn";
-		changeingame = 0;
+	class length: CUPLength {
+		values[] = {"Standard", "Compact", "DMR"};
+	};
 
-		values[] = {"compact", "standard", "dmr"};
-
-		class compact {
-			label = "$STR_ACEAX_CUP_compact_dn";
-			image="#(rgb,8,8,3)color(0.1,0.1,0.1,1)";
-		};
-
-		class standard {
-			label = "$STR_ACEAX_CUP_standard_dn";
-			image="#(rgb,8,8,3)color(0.1,0.1,0.1,1)";
-		};
-
-		class dmr {
-			label = "$STR_ACEAX_CUP_dmr_dn";
-			image="#(rgb,8,8,3)color(0.1,0.1,0.1,1)";
-		};
+	class ugl: CUPUGL {
+		values[] = {"None", "EGLM"};
 	};
 };
