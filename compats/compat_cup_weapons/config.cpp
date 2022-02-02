ecf8454a9804b6615db0d395f0239adbb0a952ef
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2022-02-02 15:12:36 +0000

    feat: Add authors and headers.

--- compats/compat_cup_weapons/config.cpp
+++ compats/compat_cup_weapons/config.cpp
@@ -1,5 +1,8 @@
 #include "script_component.hpp"
 
+#define AUTHOR ECSTRING(COMPONENT,author)
+#define DEFAULT_AUTHOR author = AUTHOR
+
 class CfgPatches {
     class ADDON {
         name = QUOTE(COMPONENT);
@@ -7,10 +10,13 @@ class CfgPatches {
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"CUP_Weapons_LoadOrder", "aceax_gearinfo"};
-        author = "AUTHOR";
+        DEFAULT_AUTHOR;
         VERSION_CONFIG;
     };
 };
 
 #include "models\XtdGearModels.hpp"
 #include "infos\XtdGearInfos.hpp"
+
+#undef DEFAULT_AUTHOR
+#undef AUTHOR
