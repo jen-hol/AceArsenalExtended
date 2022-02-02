3bf95afadf62c5df69791cc6e476b6d671c9e0dc
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2022-02-02 15:13:00 +0000

    feat: more macros for headers.

--- compats/compat_cup_weapons/models/XtdGearModels.hpp
+++ compats/compat_cup_weapons/models/XtdGearModels.hpp
@@ -1,6 +1,12 @@
 class XtdGearModels {
 	#include "_options.hpp"
 
+	#define M(NAME) CUP_##NAME
+	#define LBL(OPTION) ECSTRING(model_dn,M(OPTION))
+	#define LABEL(OPTION) label = LBL(OPTION)
+	#define HDR(OPTION) LABEL(OPTION);\
+		DEFAULT_AUTHOR
+
 	class CfgWeapons {
 		#include "acr.hpp"
 		#include "hk417.hpp"
@@ -8,4 +14,9 @@ class XtdGearModels {
 
 		#include "ak100.hpp"
 	};
+
+	#undef HDR
+	#undef LABEL
+	#undef LBL
+	#undef M
 };
