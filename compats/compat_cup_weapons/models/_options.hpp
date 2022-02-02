d0aa5d0ca047e13abf5336f20561b511f980ad1f
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2022-02-02 15:10:32 +0000

    feat: Buncha macros for creating options.

--- compats/compat_cup_weapons/models/_options.hpp
+++ compats/compat_cup_weapons/models/_options.hpp
@@ -1,82 +1,100 @@
+#define LBL(OPTION) ECSTRING(option_value_dn,OPTION)
+#define DSC(OPTION) ECSTRING(option_value_dss,OPTION)
+#define LABEL(OPTION) label = LBL(OPTION)
+#define DESC(OPTION) description = DSC(OPTION)
+
+#define OLBL(OPTION) ECSTRING(option_dn,OPTION)
+#define OLABEL(OPTION) label = OLBL(OPTION)
+
 #define DEFAULT_COLOR "#(rgb,8,8,3)color(0.1,0.1,0.1,1)"
-
 #define DEFAULT_IMAGE image = DEFAULT_COLOR
-
-#define DEFAULT_CLASS(NAME) label = QUOTE(NAME);\
+#define DEFAULT_CLASS(NAME) LABEL(NAME);\
 	DEFAULT_IMAGE
 
 #define DEF(NAME) class NAME {\
 	DEFAULT_CLASS(NAME);\
+}
+#define DEFD(NAME) class NAME {\
+	DEFAULT_CLASS(NAME);\
+	DESC(NAME);\
 }
 
 class CamoBase;
 class CUPCamo: CamoBase {
 	values[] = {};
 
+	#define CAMO(CLASS,IMG) class CLASS {\
+		LABEL(CLASS);\
+		image = IMG;\
+	}
+	#define RCAMO(CLASS,FROM) class CLASS: FROM {\
+		LABEL(CLASS);\
+	}
+
 	class WHT;
-	class SNW: WHT {
-		label = "Snow";
-	};
+	RCAMO(SNW,WHT);
+	RCAMO(WNT,WHT);
 
-	class WDL {
-		label = "Woodland";
-		image = "#(rgb,8,8,3)color(0.02,0.39,0.30,1)";
-	};
+	CAMO(WDL,"#(rgb,8,8,3)color(0.02,0.39,0.30,1)");
 
-	class TAN;
-	class DST: TAN {
-		label = "Desert";
-	};
+	CAMO(TAN,DEFAULT_COLOR);
+	RCAMO(DST,TAN);
+	RCAMO(MTAN,TAN);
+
+	CAMO(LUSH,DEFAULT_COLOR);
+	CAMO(ARID,DEFAULT_COLOR);
+	CAMO(STIG,DEFAULT_COLOR);
+	CAMO(TIG,DEFAULT_COLOR);
+	CAMO(TWOC,DEFAULT_COLOR);
+	CAMO(CTRG,DEFAULT_COLOR);
+	CAMO(CTRGT,DEFAULT_COLOR);
+	CAMO(UHEX,DEFAULT_COLOR);
+	CAMO(CYT,DEFAULT_COLOR);
+
+	#undef CAMO
+	#undef RCAMO
 };
 
-#define AKMDL(NUM) class AK##NUM {\
-	label = QUOTE(AK-##NUM);\
-	DEFAULT_IMAGE;\
-}
-#define AKMDLD(NUM,DESC) class AK##NUM {\
-	label = QUOTE(AK-##NUM);\
-	description = DESC;\
-	DEFAULT_IMAGE;\
-}
-
 class CUPModel {
-	label = "Weapon Model";
+	OLABEL(CUPModel);
 	changeingame = 0;
 	values[] = {};
+
+	#define AKMDL(NUM) DEF(AK##NUM)
+	#define AKMDLD(NUM) DEFD(AK##NUM)
 
 	AKMDLD(101, "5.56 NATO");
 	AKMDLD(102, "5.56 NATO / Carbine");
 	AKMDLD(103, "7.62x39mm");
 	AKMDLD(104, "7.62x39mm / Carbine");
 	AKMDLD(105, "5.45x39mm / Carbine");
-	AKMDL(106);
 	AKMDLD(107, "5.45x39mm / 3rnd Burst");
 	AKMDLD(108, "5.56 NATO / 3rnd Burst");
 	AKMDLD(109, "7.62x39mm / 3rnd Burst");
+
+	AKMDLD(12, "5.45x39mm");
+	AKMDLD(15, "7.62x39mm");
+	AKMDLD(19, "5.56mm");
+
+	#undef AKMDL
+	#undef AKMDLD
+
+	DEFD(A1);
+	DEFD(A1G1);
+	DEFD(A2);
 };
 
-#undef AKMDL
-
 class CUPCaliber {
-	label = "Caliber";
+	OLABEL(CUPCaliber);
 	changeingame = 0;
 	values[] = {};
 
-	class Caliber556 {
-		label = "5.56";
-		description = "5.56mm";
-		DEFAULT_IMAGE;
-	};
-
-	class Caliber68 {
-		label = "6.8";
-		description = "6.8mm SPC";
-		DEFAULT_IMAGE;
-	};
+	DEFD(Caliber556);
+	DEFD(Caliber68);
 };
 
 class CUPLength {
-	label = "Barrel Length";
+	OLABEL(CUPLength);
 	changeingame = 0;
 	values[] = {};
 
@@ -84,10 +102,22 @@ class CUPLength {
 	DEF(DMR);
 	DEF(Compact);
 	DEF(CQC);
+
+	#define INCH(LEN) DEFD(Inches##LEN)
+	#define PINCH(LEN,SUB) DEFD(Inches##LEN##_##SUB)
+
+	PINCH(14,5);
+	INCH(14);
+	PINCH(11,3);
+	INCH(11);
+	INCH(8);
+
+	#undef INCH
+	#undef PINCH
 };
 
 class CUPUGL {
-	label = "Underbarrel";
+	OLABEL(CUPUGL);
 	changeingame = 0;
 	values[] = {};
 
@@ -95,30 +125,42 @@ class CUPUGL {
 	DEF(M203);
 	DEF(M320);
 	DEF(EGLM);
-	class GP25 {
-		label = "GP-25";
-		DEFAULT_IMAGE;
-	};
+	DEF(GP25);
+	DEF(GP34);
+	DEF(AFG);
+	DEF(FG);
+	DEFD(FlashGrip);
 };
 
 class CUPRail {
-	label = "Rails";
+	OLABEL(CUPRail);
 	changeingame = 0;
 	values[] = {};
 
 	DEF(None);
-	class RIS {
-		label = "RIS";
-		description = "Sight Rail";
-		DEFAULT_IMAGE;
-	};
-	class Zentico {
-		label = "Zentico/RIS";
-		description = "Sight Rail and Side Rail";
-		DEFAULT_IMAGE;
-	};
+	DEFD(RIS);
+	DEFD(Zentico);
+	DEF(Scope);
 };
 
+class CUPHandguard {
+	OLABEL(CUPHandguard);
+	changeingame = 0;
+	values[] = {};
+
+	DEFD(Factory);
+	DEFD(Quad);
+	DEFD(MOE);
+	DEFD(RC);
+};
+
+#undef OLBL
+#undef OLABEL
+#undef DEFD
+#undef DSC
+#undef LBL
+#undef DESC
+#undef LABEL
 #undef DEF
 #undef DEFAULT_CLASS
 #undef DEFAULT_IMAGE
