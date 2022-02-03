9a5cf157847d6de6c07900f778a7a874c59298cb
jen-hol <98978121+jen-hol@users.noreply.github.com>
2022-02-03 18:39:47 +0000

    fix: Fixing class names for weapons
    Fixed class names for the following weapons:
    sa58, l115a3, l129a1, l7a2, l85, m1014, m107 and m110

--- compats/compat_cup_weapons/models/l129a1.hpp
+++ compats/compat_cup_weapons/models/l129a1.hpp
@@ -1,12 +1,59 @@
-class CUP_L129A1 {
-	HDR(L129A1);
-	options[] = {"camo", "ugl"};
+class CUP_srifle_L129A1 {
+	model = "CUP_L129A1";
+	camo = "BLK";
+	ugl = "None";
+};
 
-	class camo: CUPCamo {
-		values[] = {"BLK", "CTRGA", "CTRGT", "DST", "WDL"};
-	};
+class CUP_srifle_L129A1_ctrg {
+	model = "CUP_L129A1";
+	camo = "CTRGA";
+	ugl = "None";
+};
 
-	class ugl: CUPUGL {
-		values[] = {"None", "FG"};
-	};
+class CUP_srifle_L129A1_ctrgt {
+	model = "CUP_L129A1";
+	camo = "CTRGT";
+	ugl = "None";
+};
+
+class CUP_srifle_L129A1_d {
+	model = "CUP_L129A1";
+	camo = "DST";
+	ugl = "None";
+};
+
+class CUP_srifle_L129A1_HG_w {
+	model = "CUP_L129A1";
+	camo = "WDL";
+	ugl = "None";
+};
+
+class CUP_srifle_L129A1_HG_ctrg {
+	model = "CUP_L129A1";
+	camo = "BLK";
+	ugl = "FG";
+};
+
+class CUP_srifle_L129A1_HG {
+	model = "CUP_L129A1";
+	camo = "CTRGA";
+	ugl = "FG";
+};
+
+class CUP_srifle_L129A1_HG_ctrgt {
+	model = "CUP_L129A1";
+	camo = "CTRGT";
+	ugl = "FG";
+};
+
+class CUP_srifle_L129A1_HG_d {
+	model = "CUP_L129A1";
+	camo = "DST";
+	ugl = "FG";
+};
+
+class CUP_L129A1_Woodland_Foregrip {
+	model = "CUP_L129A1";
+	camo = "WDL";
+	ugl = "FG";
 };
--- compats/compat_cup_weapons/models/m110.hpp
+++ compats/compat_cup_weapons/models/m110.hpp
@@ -1,12 +1,35 @@
-class CUP_M110 {
-	HDR(M110);
-	options[] = {"camo", "stock"};
+class CUP_srifle_m110_kac_black {
+	model = "CUP_M110";
+	camo = "BLK";
+	stock = "Standard";
+};
 
-	class camo: CUPCamo {
-		values[] = {"BLK", "TAN", "WDL"};
-	};
+class CUP_srifle_m110_kac {
+	model = "CUP_M110";
+	camo = "TAN";
+	stock = "Standard";
+};
 
-	class stock: CUPStock {
-		values[] = {"Standard", "PRS"};
-	};
+class CUP_srifle_m110_kac_woodland {
+	model = "CUP_M110";
+	camo = "WDL";
+	stock = "Standard";
+};
+
+class CUP_srifle_M110_black {
+	model = "CUP_M110";
+	camo = "BLK";
+	stock = "PRS";
+};
+
+class CUP_srifle_M110 {
+	model = "CUP_M110";
+	camo = "TAN";
+	stock = "PRS";
+};
+
+class CUP_srifle_M110_woodland {
+	model = "CUP_M110";
+	camo = "WDL";
+	stock = "PRS";
 };
