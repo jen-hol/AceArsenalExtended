ed6966e7d5d1389c6c1bded6e1d7be30953acf81
jen-hol <rob.xav.hol@gmail.com>
2022-02-04 18:02:02 +0000

    feat: Add g3a3

---
+++ compats/compat_cup_weapons/infos/g3a3.hpp
@@ -0,0 +1,41 @@
+class CUP_arifle_G3A3_ris {
+    model = "CUP_G3A3";
+    camo = "OD";
+    handguard = "Standard";
+    ugl = "None";
+};
+
+class CUP_arifle_G3A3_ris_black { 
+    model = "CUP_G3A3";
+    camo = "BLK";
+    handguard = "Standard";
+    ugl = "None";
+};
+
+class CUP_arifle_G3A3_ris_vfg {
+    model = "CUP_G3A3";
+    camo = "OD";
+    handguard = "Standard";
+    ugl = "FG";
+};
+
+class CUP_arifle_G3A3_ris_vfg_black {
+    model = "CUP_G3A3";
+    camo = "BLK";
+    handguard = "Standard";
+    ugl = "FG";
+};
+
+class CUP_arifle_G3A3_modern_ris {
+    model = "CUP_G3A3";
+    camo = "OD";
+    handguard = "Wide";
+    ugl = "None";
+};
+
+class CUP_arifle_G3A3_modern_ris_black {
+    model = "CUP_G3A3";
+    camo = "BLK";
+    handguard = "Wide";
+    ugl = "None";
+};
