5f0f4eab2f12db8f9fd385f3ffbbfca90201626c
jen-hol <rob.xav.hol@gmail.com>
2022-02-04 21:32:19 +0000

    feat: Add mag60

---
+++ compats/compat_cup_weapons/infos/mag60.hpp
@@ -0,0 +1,14 @@
+class CUP_lmg_FNMAG {
+    rail = "None";
+    stock = "Wood";
+};
+
+class CUP_lmg_FNMAG_RIS {
+    rail = "RIS";
+    stock = "Wood";
+};
+
+class CUP_lmg_FNMAG_RIS_modern {
+    rail = "RIS";
+    stock = "Plastic";
+};
