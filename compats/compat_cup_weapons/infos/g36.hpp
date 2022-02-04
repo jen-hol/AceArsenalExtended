7987386a5a268ccb80592fdbbce19599364492fb
jen-hol <rob.xav.hol@gmail.com>
2022-02-04 21:14:36 +0000

    feat: Add g36

---
+++ compats/compat_cup_weapons/infos/g36.hpp
@@ -0,0 +1,687 @@
+class CUP_arifle_G36A {
+    model = "G36";
+    g36model = "G36";
+    camo = "BLK";
+    rail = "None";
+    ugl = "None";
+};
+
+class CUP_arifle_G36A_camo {
+    model = "G36";
+    g36model = "G36";
+    camo = "DST";
+    rail = "None";
+    ugl = "None";
+};
+
+class CUP_arifle_G36A_RIS {
+    model = "G36";
+    g36model = "G36";
+    camo = "BLK";
+    rail = "RIS";
+    ugl = "None";
+};
+
+class CUP_arifle_G36A_RIS_camo {
+    model = "G36";
+    g36model = "G36";
+    camo = "DST";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36A_RIS_wdl {
+    model = "G36";
+    g36model = "G36";
+    camo = "WDL";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36A_wdl {
+    model = "G36";
+    g36model = "G36";
+    camo = "WDL";
+    rail = "None";
+    ugl = "None";
+};
+
+class CUP_arifle_AG36 {
+    model = "G36";
+    g36model = "G36";
+    camo = "BLK";
+    rail = "None";
+    ugl = "AG36";
+};
+
+class CUP_arifle_AG36_camo {
+    model = "G36";
+    g36model = "G36";
+    camo = "DST";
+    rail = "None";
+    ugl = "AG36";
+};
+
+class CUP_arifle_G36A_AG36_RIS {
+    model = "G36";
+    g36model = "G36";
+    camo = "BLK";
+    rail = "RIS";
+    ugl = "AG36";
+}
+
+class CUP_arifle_G36A_AG36_RIS_camo {
+    model = "G36";
+    g36model = "G36";
+    camo = "DST";
+    rail = "RIS";
+    ugl = "AG36";
+};
+
+class CUP_arifle_G36A_AG36_RIS_wdl {
+    model = "G36";
+    g36model = "G36";
+    camo = "WDL";
+    rail = "RIS";
+    ugl = "AG36";
+};
+
+class CUP_arifle_AG36_wdl {
+    model = "G36";
+    g36model = "G36";
+    camo = "WDL";
+    rail = "None";
+    ugl = "AG36";
+};
+
+class CUP_arifle_G36A3 {
+    model = "G36";
+    g36model = "G36A3";
+    camo = "BLK";
+    rail = "RIS";
+    ugl = "None";
+};
+
+class CUP_arifle_G36A3_camo {
+    model = "G36";
+    g36model = "G36A3";
+    camo = "DST";
+    rail = "RIS";
+    ugl = "None";
+};
+
+class CUP_arifle_G36A3_grip {
+    model = "G36";
+    g36model = "G36A3";
+    camo = "BLK";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36A3_grip_camo {
+    model = "G36";
+    g36model = "G36A3";
+    camo = "DST";
+    rail = "RIS";
+    ugl = "FG":
+};
+
+class CUP_arifle_G36A3_grip_hex {
+    model = "G36";
+    g36model = "G36A3";
+    camo = "HEX";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36A3_grip_wdl {
+    model = "G36";
+    g36model = "G36A3";
+    camo = "WDL";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36A3_hex {
+    model = "G36";
+    g36model = "G36A3";
+    camo = "HEX";
+    rail = "RIS";
+    ugl = "None";
+};
+
+class CUP_arifle_G36A3_wdl {
+    model = "G36";
+    g36model = "G36A3";
+    camo = "WDL";
+    rail = "RIS";
+    ugl = "None";
+};
+
+class CUP_arifle_G36A3_AG36 {
+    model = "G36";
+    g36model = "G36A3";
+    camo = "BLK";
+    rail = "RIS";
+    ugl = "AG36";
+};
+
+class CUP_arifle_G36A3_AG36_camo {
+    model = "G36";
+    g36model = "G36A3";
+    camo = "DST";
+    rail = "RIS";
+    ugl = "AG36";
+};
+
+class CUP_arifle_G36A3_AG36_hex {
+    model = "G36";
+    g36model = "G36A3";
+    camo = "HEX";
+    rail = "RIS";
+    ugl = "AG36";
+};
+
+class CUP_arifle_G36A3_AG36_wdl {
+    model = "G36";
+    g36model = "G36A3";
+    camo = "WDL";
+    rail = "RIS";
+    ugl = "AG36";
+};
+
+class CUP_arifle_G36C {
+    model = "G36";
+    g36model = "G36C";
+    camo = "BLK";
+    rail = "RIS";
+    ugl = "None";
+};
+
+class CUP_arifle_G36C_VFG_Carry {
+    model = "G36";
+    g36model = "G36C";
+    camo = "BLK";
+    rail = "None";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36C_camo {
+    model = "G36";
+    g36model = "G36C";
+    camo = "DST";
+    rail = "RIS";
+    ugl = "None";
+};
+
+class CUP_arifle_G36C_VFG {
+    model = "G36";
+    g36model = "G36C";
+    camo = "BLK";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36C_VFG_camo {
+    model = "G36";
+    g36model = "G36C";
+    camo = "DST";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36C_VFG_hex {
+    model = "G36";
+    g36model = "G36C";
+    camo = "HEX";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36C_VFG_wdl {
+    model = "G36";
+    g36model = "G36C"
+    camo = "WDL";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36C_hex {
+    model = "G36";
+    g36model = "G36C";
+    camo = "HEX";
+    rail = "RIS";
+    ugl = "None";
+};
+
+class CUP_arifle_G36C_wdl {
+    model = "G36";
+    g36model = "G36C";
+    camo = "WDL";
+    rail = "RIS";
+    ugl = "None";
+};
+
+class CUP_arifle_G36CA3 {
+    model = "G36";
+    g36model = "G36CA3";
+    camo = "BLK";
+    rail = "RIS";
+    ugl = "None";
+};
+
+class CUP_arifle_G36CA3_afg {
+    model = "G36";
+    g36model = "G36CA3";
+    camo = "BLK";
+    rail = "RIS";
+    ugl = "AFG";
+};
+
+class CUP_arifle_G36CA3_afg_camo {
+    model = "G36";
+    g36model = "G36CA3";
+    camo = "DST";
+    rail = "RIS";
+    ugl = "AFG";
+};
+
+class CUP_arifle_G36CA3_afg_hex {
+    model = "G36";
+    g36model = "G36CA3";
+    camo = "HEX";
+    rail = "RIS";
+    ugl = "AFG";
+};
+
+class CUP_arifle_G36CA3_afg_wdl {
+    model = "G36";
+    g36model = "G36CA3";
+    camo = "WDL";
+    rail = "RIS";
+    ugl = "AFG";
+};
+
+class CUP_arifle_G36CA3_camo {
+    model = "G36";
+    g36model = "G36CA3";
+    camo = "DST";
+    rail = "RIS";
+    ugl = "AFG";
+};
+
+class CUP_arifle_G36CA3_grip {
+    model = "G36";
+    g36model = "G36CA3";
+    camo = "BLK";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36CA3_grip_camo {
+    model = "G36";
+    g36model = "G36CA3";
+    camo = "DST";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36CA3_grip_hex {
+    model = "G36";
+    g36model = "G36CA3";
+    camo = "HEX";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36CA3_grip_wdl {
+    model = "G36";
+    g36model = "G36CA3";
+    camo = "WDL";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36CA3_hex {
+    model = "G36";
+    g36model = "G36CA3";
+    camo = "HEX";
+    rail = "RIS";
+    ugl = "None";
+};
+
+class CUP_arifle_G36CA3_wdl {
+    model = "G36";
+    g36model = "G36CA3";
+    camo = "WDL";
+    rail = "RIS";
+    ugl = "None";
+};
+
+class CUP_arifle_G36E {
+    model = "G36";
+    g36model = "G36E";
+    camo = "BLK";
+    rail = "None";
+    ugl = "None";
+};
+
+class CUP_arifle_G36E_camo {
+    model = "G36";
+    g36model = "G36E";
+    camo = "DST";
+    rail = "None";
+    ugl = "None";
+};
+
+class CUP_arifle_G36E_wdl {
+    model = "G36";
+    g36model = "G36E";
+    camo = "WDL";
+    rail = "None";
+    ugl = "None";
+};
+
+class CUP_arifle_G36K {
+    model = "G36";
+    g36model = "G36K";
+    camo = "BLK";
+    rail = "None";
+    ugl = "None";
+};
+
+class CUP_arifle_G36K_KSK {
+    model = "G36";
+    g36model = "G36KSK";
+    camo = "BLK";
+    rail = "RIS";
+    ugl = "None";
+};
+
+class CUP_arifle_G36K_KSK_AFG {
+    model = "G36";
+    g36model = "G36KSK";
+    camo = "BLK";
+    rail = "RIS";
+    ugl = "AFG";
+};
+
+class CUP_arifle_G36K_KSK_AFG_camo {
+    model = "G36";
+    g36model = "G36KSK";
+    camo = "DST";
+    rail = "RIS"
+    ugl = "AFG";
+};
+
+class CUP_arifle_G36K_KSK_AFG_hex {
+    model = "G36";
+    g36model = "G36KSK";
+    camo = "HEX";
+    rail = "RIS";
+    ugl = "AFG";
+};
+
+class CUP_arifle_G36K_KSK_camo {
+    model = "G36";
+    g36model = "G36KSK";
+    camo = "DST";
+    rail = "RIS";
+    ugl = "None";
+};
+
+class CUP_arifle_G36K_KSK_VFG {
+    model = "G36";
+    g36model = "G36KSK";
+    camo = "BLK";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36K_KSK_VFG_camo {
+    model = "G36";
+    g36model = "G36KSK";
+    camo = "DST";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36K_KSK_VFG_hex {
+    model = "G36";
+    g36model = "G36KSK";
+    camo = "HEX";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36K_KSK_hex {
+    model = "G36";
+    g36model = "G36KSK";
+    camo = "HEX";
+    rail = "RIS";
+    ugl = "None";
+};
+
+class CUP_arifle_G36K_camo {
+    model = "G36";
+    g36model = "G36K";
+    camo = "DST";
+    rail = "None";
+    ugl = "None";
+};
+
+class CUP_arifle_G36K_VFG {
+    model = "G36";
+    g36model = "G36K";
+    camo = "BLK";
+    rail = "None";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36K_VFG_camo {
+    model = "G36";
+    g36model = "G36K";
+    camo = "DST";
+    rail = "None";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36K_VFG_wdl {
+    model = "G36";
+    g36model = "G36K";
+    camo = "WDL";
+    rail = "None";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36K_RIS {
+    model = "G36";
+    g36model = "G36K";
+    camo = "BLK";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36K_RIS_camo {
+    model = "G36";
+    g36model = "G36K";
+    camo = "DST";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36K_RIS_hex {
+    model = "G36";
+    g36model = "G36K";
+    camo = "HEX";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36K_RIS_wdl {
+    model = "G36";
+    g36model = "G36K";
+    camo = "WDL";
+    rail = "RIS";
+    ugl = "FG";
+};
+
+class CUP_arifle_G36K_wdl {
+    model = "G36";
+    g36model = "G36K";
+    camo = "WDL";
+    rail = "None";
+    ugl = "None";
+};
+
+class CUP_arifle_G36K_AG36 {
+    model = "G36";
+    g36model = "G36K";
+    camo = "BLK";
+    rail = "None";
+    ugl = "AG36";
+};
+
+class CUP_arifle_G36K_AG36_camo {
+    model = "G36";
+    g36model = "G36K";
+    camo = "DST";
+    rail = "None";
+    ugl = "AG36";
+};
+
+class CUP_arifle_G36K_RIS_AG36 {
+    model = "G36";
+    g36model = "G36K";
+    camo = "BLK";
+    rail = "RIS";
+    ugl = "AG36";
+};
+
+class CUP_arifle_G36K_RIS_AG36_camo {
+    model = "G36";
+    g36model = "G36K";
+    camo = "DST";
+    rail = "RIS";
+    ugl = "AG36";
+};
+
+class CUP_arifle_G36K_RIS_AG36_hex {
+    model = "G36";
+    g36model = "G36K";
+    camo = "HEX";
+    rail = "RIS";
+    ugl = "AG36";
+};
+
+class CUP_arifle_G36K_RIS_AG36_wdl {
+    model = "G36";
+    g36model = "G36K";
+    camo = "WDL";
+    rail = "RIS";
+    ugl = "AG36";
+};
+
+class CUP_arifle_G36K_AG36_wdl {
+    model = "G36";
+    g36model = "G36K";
+    camo = "WDL";
+    rail = "None";
+    ugl = "AG36";
+};
+
+class CUP_arifle_G36KA3 {
+    model = "G36";
+    g36model = "G36KA3";
+    camo = "BLK";
+    rail = "None";
+    ugl = "None";
+};
+
+class CUP_arifle_G36KA3_afg {
+    model = "G36";
+    g36model = "G36KA3";
+    camo = "BLK";
+    rail = "None";
+    ugl = "AFG";
+};
+
+class CUP_arifle_G36KA3_afg_camo {
+    model = "G36";
+    g36model = "G36KA3";
+    camo = "DST";
+    rail = "None";
+    ugl = "AFG";
+};
+
+class CUP_arifle_G36KA3_afg_hex {
+    model = "G36";
+    g36model = "G36KA3";
+    camo = "HEX";
+    rail = "None";
+    ugl = "AFG";
+};
+
+class CUP_arifle_G36KA3_afg_wdl {
+    model = "G36";
+    g36model = "G36KA3";
+    camo = "WDL";
+    rail = "None";
+    ugl = "AFG";
+};
+
+class CUP_arifle_G36KA3_camo {
+    model = "G36";
+    g36model = "G36KA3";
+    camo = "DST";
+    rail = "None";
+    ugl = "None";
+};
+
+class CUP_arifle_G36KA3_grip {
+    model = "G36";
+    g36model = "G36KA3";
+    camo = "BLK";
+    rail = "None";
+    ugl = "VFG";
+};
+
+class CUP_arifle_G36KA3_grip_camo {
+    model = "G36";
+    g36model = "G36KA3";
+    camo = "DST";
+    rail = "None";
+    ugl = "VFG";
+};
+
+class CUP_arifle_G36KA3_grip_hex {
+    model = "G36";
+    g36model = "G36KA3";
+    camo = "HEX";
+    rail = "None";
+    ugl = "VFG";
+};
+
+class CUP_arifle_G36KA3_grip_wdl {
+    model = "G36";
+    g36model = "G36KA3";
+    camo = "WDL";
+    rail = "None";
+    ugl = "VFG";
+};
+
+class CUP_arifle_G36KA3_hex {
+    model = "G36";
+    g36model = "G36KA3";
+    camo = "HEX";
+    rail = "None";
+    ugl = "None";
+};
+
+class CUP_arifle_G36KA3_wdl {
+    model = "G36";
+    g36model = "G36KA3";
+    camo = "WDL";
+    rail = "None";
+    ugl = "None";
+};
