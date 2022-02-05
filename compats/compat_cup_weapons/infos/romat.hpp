866d172bf06b81a5435d02a40d1c605abddbad5b
jen-hol <rob.xav.hol@gmail.com>
2022-02-05 07:53:51 +0000

    feat: Added romat

---
+++ compats/compat_cup_weapons/infos/romat.hpp
@@ -0,0 +1,9 @@
+class CUP_arifle_IMI_Romat {
+    model = "CUP_Romat";
+    rail = "None";
+};
+
+class CUP_arifle_IMI_Romat_railed {
+    model = "CUP_Romat";
+    rail = "RIS";
+};
---
+++ compats/compat_cup_weapons/models/romat.hpp
@@ -0,0 +1,8 @@
+class CUP_Romat {
+    HDR(Romat);
+    options[] = {"rail"};
+
+    class rail: CUPRail {
+        values[] = {"None", "RIS"};
+    };
+};
