8d689a63a103eaaae9072e31e2ea3763277d29a7
jen-hol <rob.xav.hol@gmail.com>
2022-02-05 06:37:08 +0000

    fix: Added model to MK16

--- compats/compat_cup_weapons/infos/mk16.hpp
+++ compats/compat_cup_weapons/infos/mk16.hpp
@@ -1,4 +1,5 @@
 class CUP_arifle_Mk16_CQC {
+    model = "CUP_MK16";
     length = "CQC";
     ugl = "None";
     camo = "TAN";
@@ -11,186 +12,217 @@ class CUP_arifle_Mk16_CQC_AFG {
 };
 
 class CUP_arifle_Mk16_CQC_AFG_black {
+    model = "CUP_MK16";
     length = "CQC";
     ugl = "AFG";
     camo = "BLK";
 };
 
 class CUP_arifle_Mk16_CQC_AFG_woodland {
+    model = "CUP_MK16";
     length = "CQC";
     ugl = "AFG";
     camo = "WDL";
 };
 
 class CUP_arifle_Mk16_CQC_black {
+    model = "CUP_MK16";
     length = "CQC";
     ugl = "None";
     camo = "BLK";
 };
 
 class CUP_arifle_Mk16_CQC_FG {
+    model = "CUP_MK16";
     length = "CQC";
     ugl = "FG";
     camo = "TAN";
 };
 
 class CUP_arifle_Mk16_CQC_FG_black {
+    model = "CUP_MK16";
     length = "CQC";
     ugl = "FG";
     camo = "BLK";
 };
 
 class CUP_arifle_Mk16_CQC_FG_woodland {
+    model = "CUP_MK16";
     length = "CQC";
     ugl = "FG";
     camo = "WDL";
 };
 
 class CUP_arifle_Mk16_CQC_SFG {
+    model = "CUP_MK16";
     length = "CQC";
     ugl = "Surefire";
     camo = "TAN";
 };
 
 class CUP_arifle_Mk16_CQC_SFG_black {
+    model = "CUP_MK16";
     length = "CQC";
     ugl = "Surefire";
     camo = "BLK";
 };
 
 class CUP_arifle_Mk16_CQC_SFG_woodland {
+    model = "CUP_MK16";
     length = "CQC";
     ugl = "Surefire";
     camo = "WDL";
 };
 
 class CUP_arifle_Mk16_CQC_woodland {
+    model = "CUP_MK16";
     length = "CQC";
     ugl = "None";
     camo = "WDL";
 };
 
 class CUP_arifle_Mk16_CQC_EGLM {
+    model = "CUP_MK16";
     length = "CQC";
     ugl = "EGLM";
     camo = "TAN";
 };
 
 class CUP_arifle_Mk16_CQC_EGLM_black {
+    model = "CUP_MK16";
     length = "CQC";
     ugl = "EGLM";
     camo = "BLK";
 };
 
 class CUP_arifle_Mk16_CQC_EGLM_woodland {
+    model = "CUP_MK16";
     length = "CQC";
     ugl = "EGLM";
     camo = "WDL";
 };
 
 class CUP_arifle_Mk16_STD {
+    model = "CUP_MK16";
     length = "Standard";
     ugl = "None";
     camo = "TAN";
 };
 
 class CUP_arifle_Mk16_STD_AFG {
+    model = "CUP_MK16";
     length = "Standard";
     ugl = "AFG";
     camo = "TAN";
 };
 
 class CUP_arifle_Mk16_STD_AFG_black {
+    model = "CUP_MK16";
     length = "Standard";
     ugl = "AFG";
     camo = "BLK";
 };
 
 class CUP_arifle_Mk16_STD_AFG_woodland {
+    model = "CUP_MK16";
     length = "Standard";
     ugl = "AFG";
     camo = "WDL";
 };
 
 class CUP_arifle_Mk16_STD_black {
+    model = "CUP_MK16";
     length = "Standard";
     ugl = "None";
     camo = "BLK";
 };
 
 class CUP_arifle_Mk16_STD_FG {
+    model = "CUP_MK16";
     length = "Standard";
     ugl = "FG";
     camo = "TAN";
 };
 
 class CUP_arifle_Mk16_STD_FG_black {
+    model = "CUP_MK16";
     length = "Standard";
     ugl = "FG";
     camo = "BLK";
 };
 
 class CUP_arifle_Mk16_STD_FG_woodland {
+    model = "CUP_MK16";
     length = "Standard";
     ugl = "FG";
     camo = "WDL";
 };
 
 class CUP_arifle_Mk16_STD_SFG {
+    model = "CUP_MK16";
     length = "Standard";
     ugl = "Surefire";
     camo = "TAN";
 };
 
 class CUP_arifle_Mk16_STD_SFG_black {
+    model = "CUP_MK16";
     length = "Standard";
     ugl = "Surefire";
     camo = "BLK";
 };
 
 class CUP_arifle_Mk16_STD_SFG_woodland {
+    model = "CUP_MK16";
     length = "Standard";
     ugl = "Surefire";
     camo = "WDL";
 };
 
 class CUP_arifle_Mk16_STD_woodland {
+    model = "CUP_MK16";
     length = "Standard";
     ugl = "None";
     camo = "WDL";
 };
 
 class CUP_arifle_Mk16_STD_EGLM {
+    model = "CUP_MK16";
     length = "Standard";
     ugl = "EGLM";
     camo = "TAN";
 };
 
 class CUP_arifle_Mk16_STD_EGLM_black {
+    model = "CUP_MK16";
     length = "Standard";
     ugl = "EGLM";
     camo = "BLK";
 };
 
 class CUP_arifle_Mk16_STD_EGLM_woodland {
+    model = "CUP_MK16";
     length = "Standard";
     ugl = "EGLM";
     camo = "WDL";
 };
 
 class CUP_arifle_Mk16_SV {
+    model = "CUP_MK16";
     length = "SV";
     ugl = "None";
     camo = "TAN";
 };
 
 class CUP_arifle_Mk16_SV_black {
+    model = "CUP_MK16";
     length = "SV";
     ugl = "None";
     camo = "BLK";
 };
 
 class CUP_arifle_Mk16_SV_woodland {
+    model = "CUP_MK16";
     length = "SV";
     ugl = "None";
     camo = "WDL";
