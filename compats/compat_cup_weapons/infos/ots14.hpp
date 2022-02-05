37bf5d64279ac8911466878079eef18e7aa49042
jen-hol <rob.xav.hol@gmail.com>
2022-02-05 07:25:11 +0000

    feat: Add OTs14

---
+++ compats/compat_cup_weapons/infos/ots14.hpp
@@ -0,0 +1,41 @@
+class CUP_arifle_OTS14_GROZA_762_Grip {
+    model = "CUP_OTs14";
+    caliber = "Caliber762_39";
+    ugl = "None";
+    handguard = "Extended";
+};
+
+class CUP_arifle_OTS14_GROZA_762 {
+    model = "CUP_OTs14";
+    caliber = "Caliber762_39";
+    ugl = "None";
+    handguard = "Standard";
+};
+
+class CUP_arifle_OTS14_GROZA_762_GL {
+    model = "CUP_OTs14";
+    caliber = "Caliber762_39";
+    ugl = "GP25";
+    handguard = "None";
+};
+
+class CUP_arifle_OTS14_GROZA_Grip {
+    model = "CUP_OTs14";
+    caliber = "Caliber939";
+    ugl = "None";
+    handguard = "Extended";
+};
+
+class CUP_arifle_OTS14_GROZA {
+    model = "CUP_OTs14";
+    caliber = "Caliber939";
+    ugl = "None";
+    handguard = "Standard";
+};
+
+class CUP_arifle_OTS14_GROZA_GL {
+    model = "CUP_OTs14";
+    caliber = "Caliber939";
+    ugl = "GP25";
+    handguard = "Standard";
+};
