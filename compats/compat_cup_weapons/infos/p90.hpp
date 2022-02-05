1840d4c4ed023b9fe88c58fe77f26e3c50ace396
jen-hol <rob.xav.hol@gmail.com>
2022-02-05 07:30:06 +0000

    feat: Added p90

---
+++ compats/compat_cup_weapons/infos/p90.hpp
@@ -0,0 +1,9 @@
+class CUP_smg_p90_black {
+    model = "CUP_P90";
+    camo = "BLK";
+};
+
+class CUP_smg_p90_olive {
+    model = "CUP_P90";
+    camo = "OD";
+};
---
+++ compats/compat_cup_weapons/models/p90.hpp
@@ -0,0 +1,8 @@
+class CUP_P90{
+    HDR(P90);
+    options[] = {"camo"};
+
+    class camo: CUPCamo{
+        values[] = {"BLK", "OD"};
+    };
+};
