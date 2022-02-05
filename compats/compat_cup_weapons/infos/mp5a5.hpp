ceacd624d51d7178189ded7e51767c9602e9e476
jen-hol <rob.xav.hol@gmail.com>
2022-02-05 07:14:12 +0000

    feat: Add mp5a5

---
+++ compats/compat_cup_weapons/infos/mp5a5.hpp
@@ -0,0 +1,23 @@
+class CUP_smg_MP5A5 {
+    model = "CUP_MP5A5";
+    rail = "None";
+    ugl = "None";
+};
+
+class CUP_smg_MP5A5_Rail {
+    model = "CUP_MP5A5";
+    rail = "Front";
+    ugl = "None";
+};
+
+class CUP_smg_MP5A5_Rail_AFG {
+    model = "CUP_MP5A5";
+    rail = "Front";
+    ugl = "AFG";
+};
+
+class CUP_smg_MP5A5_Rail_VFG {
+    model = "CUP_MP5A5";
+    rail = "Front";
+    ugl = "FG";
+};
