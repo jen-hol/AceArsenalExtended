52b17dfd52fbfdd4021334769197ef972b3bddbc
jen-hol <rob.xav.hol@gmail.com>
2022-02-05 07:20:53 +0000

    feat: Added mp7a1

---
+++ compats/compat_cup_weapons/infos/mp7a1.hpp
@@ -0,0 +1,14 @@
+class CUP_smg_MP7 {
+    model = "CUP_MP7A1";
+    camo = "BLK";
+};
+
+class CUP_smg_MP7_desert {
+    model = "CUP_MP7A1";
+    camo = "DST";
+};
+
+class CUP_smg_MP7_woodland {
+    model = "CUP_MP7A1";
+    camo = "WDL";
+};
